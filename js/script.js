ca307924ade5877f9cb45b721f3611f4102fc6fd
Filippo Bristot <filippo.bristot@hotmail.it>
2021-12-06 08:05:39 +0100

    feat(countdown): add error states

--- js/script.js
+++ js/script.js
@@ -8,13 +8,14 @@ const day = 24 * hour;
 const animationDuration = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--animation-duration')) * 1000;
 
 /* =================================== § PROJECT TIME VARS === */
-const deadline = new Date('December 8, 2021 00:00:00');
+const deadline = new Date('December 6, 2021 08:05:00');
 
 /* =================================== § DOM === */
 const containerDaysEl = document.getElementById('containerDays');
 const containerHoursEl = document.getElementById('containerHours');
 const containerMinutesEl = document.getElementById('containerMinutes');
 const containerSecondsEl = document.getElementById('containerSeconds');
+const countdownError = document.getElementById('countdownError');
 
 /* =================================== § SUPPORT FUNCTIONS === */
 function addZero(n) {
@@ -38,7 +39,8 @@ function changeAttribute(element, attribute, change) {
 /* =================================== § COUNTDOWN FUNCTION === */
 function countdown() {
   const now = new Date();
-  const rawGap = deadline - now;
+  const rawGap = deadline - now > 0 ? deadline - now : 0;
+  console.log(rawGap);
 
   /* ····················· § CURRENT ··· */
   const gapDays = Math.floor(rawGap / day);
@@ -52,16 +54,18 @@ function countdown() {
   const gapMinutesNext = Math.floor(((rawGap - minute) % hour) / minute);
   const gapSecondsNext = Math.floor(((rawGap - second) % minute) / second);
 
-  changeAttribute(containerDaysEl, 'data-current-day', addZero(gapDays));
-  changeAttribute(containerDaysEl, 'data-next-day', addZero(gapDaysNext));
-  changeAttribute(containerHoursEl, 'data-current-hour', addZero(gapHours));
-  changeAttribute(containerHoursEl, 'data-next-hour', addZero(gapHoursNext));
-  changeAttribute(containerMinutesEl, 'data-current-minute', addZero(gapMinutes));
-  changeAttribute(containerMinutesEl, 'data-next-minute', addZero(gapMinutesNext));
-  changeAttribute(containerSecondsEl, 'data-current-second', addZero(gapSeconds));
-  changeAttribute(containerSecondsEl, 'data-next-second', addZero(gapSecondsNext));
+  if (gapDays > 1000) {
+    countdownError.classList.add('show');
+  } else {
+    changeAttribute(containerDaysEl, 'data-current-day', addZero(gapDays));
+    changeAttribute(containerDaysEl, 'data-next-day', addZero(gapDaysNext));
+    changeAttribute(containerHoursEl, 'data-current-hour', addZero(gapHours));
+    changeAttribute(containerHoursEl, 'data-next-hour', addZero(gapHoursNext));
+    changeAttribute(containerMinutesEl, 'data-current-minute', addZero(gapMinutes));
+    changeAttribute(containerMinutesEl, 'data-next-minute', addZero(gapMinutesNext));
+    changeAttribute(containerSecondsEl, 'data-current-second', addZero(gapSeconds));
+    changeAttribute(containerSecondsEl, 'data-next-second', addZero(gapSecondsNext));
+  }
 }
-
-countdown();
 
 setInterval(countdown, second);
